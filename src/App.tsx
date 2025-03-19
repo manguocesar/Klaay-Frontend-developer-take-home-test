efb4d50e4d30181815263f0468031f6b4daf326b
Cesar <cesar.hertz@icloud.com>
2025-03-19 13:40:31 +0100

    Add ErrorBoundary and ErrorFallback component for improved error handling

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import { LogOut } from './components/LogOut';
 import { ProtectedRoute } from './components/ProtectedRoute';
 import { useAuth } from './context/AuthContext';
 import { ChatProvider } from './context/ChatContext';
+import { ErrorFallback } from './components/ErrorFallback';
+import { ErrorBoundary } from 'react-error-boundary';
 
 const Login = lazy(() => import('./pages/login'));
 const Conversations = lazy(() => import('./pages/conversation'));
@@ -15,30 +17,32 @@ const App: FC = () => {
   const { token } = useAuth();
 
   return (
-    <div className="flex flex-col min-h-screen items-center  bg-gray-100 w-full">
-      {token ? <LogOut /> : null}
-      <Header />
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route
-            index
-            element={token ? <Navigate to="/conversations" /> : <Navigate to="/login" />}
-          />
-          <Route path="/login" element={<Login />} />
-          <Route
-            path="/conversations"
-            element={
-              <ProtectedRoute>
-                <ChatProvider>
-                  <Conversations />
-                </ChatProvider>
-              </ProtectedRoute>
-            }
-          />
-          <Route path="*" element={<ErrorPage />} />
-        </Routes>
-      </Suspense>
-    </div>
+    <ErrorBoundary FallbackComponent={ErrorFallback}>
+      <div className="flex flex-col min-h-screen items-center  bg-gray-100 w-full">
+        {token ? <LogOut /> : null}
+        <Header />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route
+              index
+              element={token ? <Navigate to="/conversations" /> : <Navigate to="/login" />}
+            />
+            <Route path="/login" element={<Login />} />
+            <Route
+              path="/conversations"
+              element={
+                <ProtectedRoute>
+                  <ChatProvider>
+                    <Conversations />
+                  </ChatProvider>
+                </ProtectedRoute>
+              }
+            />
+            <Route path="*" element={<ErrorPage />} />
+          </Routes>
+        </Suspense>
+      </div>
+    </ErrorBoundary>
   );
 };
 
